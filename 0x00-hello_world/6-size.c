f10fc34850e7ef7f0b1d0d52c5e11d1d113fe5d8
root <githinjibrian5@gmail.com>
2022-03-10 12:16:10 -0800

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * main - Entry point
+ *
+ * Description: using thr main function this program prints sizes of variables in the computer
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long lo;
+	long long llo;
+	float f;
+
+	printf("size of a char: %c byte(s)\n",sizeof(c));
+	printf("size of an int: %d byte(s)\n",sizeof(i));
+	printf("size of a long int: %ld byte(s)\n",sizeof(lo));
+	printf("size of a long long int: %lld byte(s)\n",sizeof(llo));
+	printf("size of a float: %f byte(s)\n",sizeof(f));
+
+	return (0);
+	}	
