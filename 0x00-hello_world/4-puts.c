3d4f1cf6ced72e3871eee4b5f217fca88a543fa5
root <githinjibrian5@gmail.com>
2022-03-10 10:46:33 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+ *
+ * main - Entry point
+ * char - initialize string
+ * puts() - output
+ *
+ * */
+int main()
+{
+	char puzzle[100];
+	strcpy(puzzle, "Programming is like building a multilingual puzzle");
+	puts(puzzle);
+
+	return(0);
+
+}
