73fd54074a1ce77368560cd04bf257bd33a6cb3d
root <githinjibrian5@gmail.com>
2022-03-10 11:00:34 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,16 +2,19 @@
 #include <string.h>
 
 /**
- *
  * main - Entry point
- * char - initialize string
- * puts() - output
  *
+ * char - declare variable
+ *
+ * puts() - output string
+ *
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
 	char puzzle[100];
-	strcpy(puzzle, "\"Programming is like building a multilingual puzzle");
+
+	strcpy (puzzle, "\"Programming is like building a multilingual puzzle");
 
 	puts(puzzle);
 
