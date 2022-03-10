34cdd570fc14573675efdc86a3ad1481f16f7287
root <githinjibrian5@gmail.com>
2022-03-10 11:09:40 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * printf - print out string
+ *
+ * Return - Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+		return (0);
