c1047dfb83fe22aa6d8447ffad0031df5fe01ec9
root <githinjibrian5@gmail.com>
2022-03-10 11:40:49 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,13 +6,12 @@
  *
  * printf - print out string
  *
- * Return - Always 0 (Success)
+ * return - Always 0 (Success)
  */
 
 int main(void)
 {
-       	
-	printf("with proper grammar, but the outcome is a piece of art,");
+       		printf("with proper grammar, but the outcome is a piece of art,\n");
 
-		return (0);
+	return (0);
 }
