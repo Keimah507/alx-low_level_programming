3d44f6b7e0985989af65fe2015cff8ae6212f3ce
root <githinjibrian5@gmail.com>
2022-03-15 10:29:21 -0700

    TAsk 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,14 @@
  */
 int main(void)
 {
-	int _putchar(char c)
-	{
-		char c = "_putchar";	
-		return (write(1, &c, 1));
-	}
+	char text[9] = "_putchar";
+	int i = 0;
 
-	return (0);
+	for (i = 0; i < 8; i++)
+		{
+			_putchar(text[i]);
+		}
+			_putchar('\n');
+
+			return (0);
 }
