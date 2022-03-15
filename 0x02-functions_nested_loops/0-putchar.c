571e5b5c4a21405ad6617dfc361d8e87124be8a0
root <githinjibrian5@gmail.com>
2022-03-15 09:10:47 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <main.h>
+#include <putchar.c>
+/*
+ * main - output string putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char putchar = "_putchar";
+
+	putchar(putchar);
+
+	return (0);
+}
