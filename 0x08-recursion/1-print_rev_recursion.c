e97bca050cd325a38e66879662d7f3b04b33e13f
root <githinjibrian5@gmail.com>
2022-03-30 02:24:03 -0700

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: pointer to the string
+ *
+ * return: string in reverse
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
