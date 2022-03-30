a86350703eb5b5a0d27544e00ed7b343c992f664
root <githinjibrian5@gmail.com>
2022-03-30 07:00:04 -0700

    Task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#inlcude "main.h"
+
+/**
+ * is_prime_number - checks for prime number
+ * @n: integer
+ *
+ * Return: 1, otherwise 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return(actual_prime(n, -1));
+}
+
+/**
+ * actual_prime - checks for prime no
+ * @n: integer
+ * @i:iterator
+ *
+ * Return 1, otherwise 0
+ */
+int actual_prime(intn, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i-1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,6 +9,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt(int n, int i);
 int is_prime_number(int n);
+int actual_prime(int n, int i);
 int _putchar(char c);
 
 #endif /*MAIN_H*/
