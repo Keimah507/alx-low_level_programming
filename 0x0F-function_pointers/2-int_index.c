75d35b51632fde8a330329e828e4e96d5870b61f
root <githinjibrian5@gmail.com>
2022-04-13 08:46:18 -0700

    Task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * int_index - searches for an integer
+ * @size: number of elements in array
+ * @cmp: pointer to function
+ *
+ * Return: cmp, otherwise -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (arrray && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
