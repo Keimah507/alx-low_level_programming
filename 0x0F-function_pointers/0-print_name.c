903c3cef733c48d29a9d79f976cefa2f83d81b9f
Keimah507 <74242600+Keimah507@users.noreply.github.com>
2022-04-13 19:25:24 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,10 +3,9 @@
 
 /**
  * print_name - prints a name
- * @name: name to be printed
- * @f: pointer to function
+ * @name: name to print
+ * @f: pointer to the printing function
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
